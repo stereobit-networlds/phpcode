4014d9e7769c25b17255c043a41a000914dd01bf
Bill Alexiou <b.alexiou@stereobit.gr>
2015-06-19 12:19:44 +0300

    first commit

---
+++ home/cp/html/media-center/index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="<phpdac>frontpage.iso_language</phpdac>">
+	<head>
+	    <base href="<phpdac>rcserver.paramload use SHELL+urlbase</phpdac>/" />
+		<!-- Meta -->
+		<meta charset="utf-8">
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+		<meta name="description" content="<phpdac>shtags.get_page_info use descr</phpdac>">
+		<meta name="author" content="<phpdac>rcserver.paramload use INDEX+domain-name+1</phpdac> by stereobit.networlds">
+	    <meta name="keywords" content="<phpdac>shtags.get_page_info use keywords</phpdac>">
+	    <meta name="robots" content="all">
+		
+        <meta name="language" content="<phpdac>frontpage.current_language</phpdac>">
+		<meta name="revisit-after" content="5 days" />
+		<meta name="reply-to" content="<phpdac>rcserver.paramload use INDEX+e-mail+1</phpdac>" />
+		<meta name="document-classification" content="e-commerce" />
+		<meta name="document-rights" content="Copyrighted Work" />
+		<meta name="document-type" content="Public" />
+		<meta name="document-rating" content="Safe for Kids" />
+		<meta name="document-distribution" content="Global" />
+		<meta name="document-state" content="Dynamic" />
+		<meta name="Abstract" content="<phpdac>rcserver.paramload use INDEX+subtitle+1</phpdac>" />			
+
+	    <title>
+		<phpdac>shtags.get_page_info use item+NULL</phpdac>
+		<phpdac>rcserver.paramload use INDEX+company-name+1</phpdac> 		
+		</title>
+
+	    <!-- Bootstrap Core CSS -->
+	    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	    
+	    <!-- Customizable CSS -->
+	    <link rel="stylesheet" href="assets/css/main.css">
+	    <link rel="stylesheet" href="assets/css/orange.css">
+	    <link rel="stylesheet" href="assets/css/owl.carousel.css">
+		<link rel="stylesheet" href="assets/css/owl.transitions.css">
+		<link rel="stylesheet" href="assets/css/animate.min.css">
+
+	    <!-- Fonts -->
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800' rel='stylesheet' type='text/css'>
+		
+		<!-- Icons/Glyphs -->
+		<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+		
+		<!-- Favicon -->
+		<link rel="shortcut icon" href="images/favicon.ico">
+
+		<!-- HTML5 elements and media queries Support for IE8 : HTML5 shim and Respond.js -->
+		<!--[if lt IE 9]>
+			<script src="assets/js/html5shiv.js"></script>
+			<script src="assets/js/respond.min.js"></script>
+		<![endif]-->
+	</head>
+<body>
+	
+	<div class="wrapper">
+		<phpdac>frontpage.include_part use /parts/navigation/top-menu-bar.php+++media-center</phpdac>
+
+		<phpdac>frontpage.nvldac2 use headerStyle+frontpage.include_part:/parts/section/header.php|||media-center::frontpage.include_part:/parts/breadcrumb/breadcrumb.php|||media-center+frontpage.include_part:/parts/section/header-2.php|||media-center+1</phpdac>
+		
+		<phpdac>frontpage.include_part_arg use /pages/<mc_page>.php+++media-center</phpdac>
+		
+		<phpdac>frontpage.include_part use /parts/section/footer.php+++media-center</phpdac>
+	</div><!-- /.wrapper -->
+
+	<!-- JavaScripts placed at the end of the document so the pages load faster -->
+	<script src="assets/js/jquery-1.10.2.min.js"></script>
+	<script src="assets/js/jquery-migrate-1.2.1.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="http://maps.google.com/maps/api/js?sensor=false&amp;language=en"></script>
+	<script src="assets/js/gmap3.min.js"></script>
+	<script src="assets/js/bootstrap-hover-dropdown.min.js"></script>
+	<script src="assets/js/owl.carousel.min.js"></script>
+	<script src="assets/js/css_browser_selector.min.js"></script>
+	<script src="assets/js/echo.min.js"></script>
+	<script src="assets/js/jquery.easing-1.3.min.js"></script>
+	<script src="assets/js/bootstrap-slider.min.js"></script>
+    <script src="assets/js/jquery.raty.min.js"></script>
+    <script src="assets/js/jquery.prettyPhoto.min.js"></script>
+    <script src="assets/js/jquery.customSelect.min.js"></script>
+    <script src="assets/js/wow.min.js"></script>
+	<script src="assets/js/scripts.js"></script>
+
+	<phpdac>frontpage.include_part use /parts/google-analytics.php+++media-center</phpdac>
+	<!-- e-Enterprise, stereobit.networlds (phpdac5) -->
+</body>
+</html>
---
+++ home/cp/html/media-center/pages/home.php
@@ -0,0 +1,23 @@
+<div id="top-banner-and-menu">
+	<div class="container">
+		
+		<div class="col-xs-12 col-sm-4 col-md-3 sidemenu-holder">
+			<phpdac>frontpage.include_part use /parts/navigation/sidemenu.php+++media-center</phpdac>
+		</div><!-- /.sidemenu-holder -->
+
+		<div class="col-xs-12 col-sm-8 col-md-9 homebanner-holder">
+			<phpdac>frontpage.include_part use /parts/section/home-page-slider.php+++media-center</phpdac>	
+		</div><!-- /.homebanner-holder -->
+
+	</div><!-- /.container -->
+</div><!-- /#top-banner-and-menu -->
+
+<phpdac>frontpage.include_part use /parts/section/home-banners.php+++media-center</phpdac>
+
+<phpdac>frontpage.include_part use /parts/section/home-page-tabs.php+++media-center</phpdac>
+
+<phpdac>frontpage.include_part use /parts/section/best-sellers.php+++media-center</phpdac>
+
+<phpdac>frontpage.include_part use /parts/section/recently-viewed.php+++media-center</phpdac>
+
+<phpdac>frontpage.include_part use /parts/section/top-brands.php+++media-center</phpdac>
